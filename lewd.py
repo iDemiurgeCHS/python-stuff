5601def91b1259d2b149d289840ac39ad50ad9f3
iDemiurgeCHS <nmdstone@icloud.com>
2017-05-10 19:43:36 -0700

    Add files via upload

---
+++ lewd.py
@@ -0,0 +1,9 @@
+import random, string
+def randomSearch(legnth):
+	return''.join(random.choice(stringList))
+def getLink():
+	'''print "https://www.google.com/#safe=strict&q=" + randomSearch(10)'''
+	print "https://www.pornhub.com/video/search?search=" + randomSearch(1) + '+' + randomSearch(1)
+stringList = ['teen','anal','bob','gabe','toothless','granny','mom','son','brother','sister','alexander',
+			  'nick','dick','big','small','boobs','bdsm','hentai','schoolgirl','little','hard']
+getLink()
